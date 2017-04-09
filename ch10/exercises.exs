f9589da79ef0aa13d71504c3033c905e84e631d9
Sean Marzug-McCarthy <samdec11@gmail.com>
2017-04-09 13:27:29 -0400

    Finish chapter 10

--- ch10/exercises.exs
+++ ch10/exercises.exs
@@ -73,10 +73,40 @@ defmodule MyList do
   # ListsAndRecursion-7
   # In the last exercise of Chapter 7 on page 65, you wrote a `span` function. Use it and list
   # comprehensions to return a list of prime numbers from 2 to n.
+
   def span(from, to) when from > to, do: []
   def span(from, to), do: [from | span(from + 1, to)]
 
   def span_primes(num) do
     for x <- span(2, num), x == 2 || rem(x, 2) > 0 && rem(x, 3) > 0, do: x
+  end
+
+  # ListsAndRecursion-8
+  # The Pragmatic Bookshelf has offices in Texas (TX) and North Carolina (NC), so we have to charge
+  # sales tax on orders shipped to these states. The rates can be expressed as a keyword list:
+  #
+  tax_rates = [ NC: 0.075, TX: 0.08 ]
+  #
+  # Here's a list of orders:
+  orders = [
+    [ id: 123, ship_to: :NC, net_amount: 100.00 ],
+    [ id: 124, ship_to: :OK, net_amount: 35.50 ],
+    [ id: 125, ship_to: :TX, net_amount: 24.00 ],
+    [ id: 126, ship_to: :TX, net_amount: 44.80 ],
+    [ id: 127, ship_to: :NC, net_amount: 25.00 ],
+    [ id: 128, ship_to: :MA , net_amount: 10.00 ],
+    [ id: 129, ship_to: :CA, net_amount: 102.00 ],
+    [ id: 130, ship_to: :NC, net_amount: 50.00 ]
+  ]
+  #
+  # Write a function that takes both lists and returns a copy of the orders, but with an extra
+  # field, total_amount, which is the net plus sales tax. If a shipment is not to NC or TX, there's
+  # no tax applied.
+
+  def apply_tax(orders, tax_rates) do
+    orders |> Enum.map(fn o ->
+      tax_amount = if tax_rates[o[:ship_to]], do: o[:net_amount] * tax_rates[o[:ship_to]], else: 0
+      Keyword.put(o, :total_amount, (o[:net_amount] + tax_amount))
+    end)
   end
 end
