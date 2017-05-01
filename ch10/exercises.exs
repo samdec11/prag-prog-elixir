8ac357b0328d299e110185182341ac3ebdc22ceb
Sean Marzug-McCarthy <samdec11@gmail.com>
2017-05-01 00:07:12 -0400

    More ch 11

--- ch10/exercises.exs
+++ ch10/exercises.exs
@@ -90,13 +90,13 @@ defmodule MyList do
   # Here's a list of orders:
   orders = [
     [ id: 123, ship_to: :NC, net_amount: 100.00 ],
-    [ id: 124, ship_to: :OK, net_amount: 35.50 ],
-    [ id: 125, ship_to: :TX, net_amount: 24.00 ],
-    [ id: 126, ship_to: :TX, net_amount: 44.80 ],
-    [ id: 127, ship_to: :NC, net_amount: 25.00 ],
-    [ id: 128, ship_to: :MA , net_amount: 10.00 ],
+    [ id: 124, ship_to: :OK, net_amount: 35.50  ],
+    [ id: 125, ship_to: :TX, net_amount: 24.00  ],
+    [ id: 126, ship_to: :TX, net_amount: 44.80  ],
+    [ id: 127, ship_to: :NC, net_amount: 25.00  ],
+    [ id: 128, ship_to: :MA, net_amount: 10.00  ],
     [ id: 129, ship_to: :CA, net_amount: 102.00 ],
-    [ id: 130, ship_to: :NC, net_amount: 50.00 ]
+    [ id: 130, ship_to: :NC, net_amount: 50.00  ]
   ]
   #
   # Write a function that takes both lists and returns a copy of the orders, but with an extra
--- ch11/exercises.exs
+++ ch11/exercises.exs
@@ -33,4 +33,67 @@ defmodule MyModule do
   # returns the result of the calculation. The individual numbers do not have leading plus or minus
   # signs.
   # calculate('123 + 27') # => 150
+  def calculate(xs) do
+    [num1, operator, num2] = to_string(xs) |> String.split
+
+    apply Kernel, String.to_atom(operator), Enum.map([num1, num2], &String.to_integer/1)
+  end
+
+  # StringsAndBinaries-5
+  # Write a function that takes a list of dqs and prints each on a separate line, centered in a
+  # column that has the width of the longest string. Make sure it works with UTF characters.
+  # iex> center(["cat", "zebra", "elephant"])
+  #   cat
+  #  zebra
+  # elephant
+  def center(list_of_strings) do
+    max_length = list_of_strings
+    |> Enum.max_by(&String.length/1)
+    |> String.length
+
+    list_of_strings
+    |> Enum.each(&(print_centered(&1, max_length)))
+  end
+
+  defp print_centered(string, max_length) do
+    padding = String.duplicate(" ", div(max_length - String.length(string), 2))
+    IO.write padding
+    IO.puts string
+  end
+
+  # StringsAndBinaries-6
+  # Write a function to capitalize the sentences in a string. Each sentence is terminated by a
+  # period and a pace. Right now, the case of the characters in the string is random.
+  # iex> capitalize_sentences("oh. a DOG. woof. ")
+  # "Oh. A dog. Woof. "
+  def capitalize_sentences(str) do
+    str
+    |> String.split(". ")
+    |> Enum.map(&String.capitalize/1)
+    |> Enum.join(". ")
+  end
+
+  # StringsAndBinaries-7
+  # Chapter 10 had an exercise about calculating sales tax on page 110. We now have the sales
+  # information in a file of comma-separated id, ship_to, and amount values. The file looks like
+  # this:
+  # id,ship_to,net_amount
+  # 123,:NC,100.00
+  # 124,:OK,35.50
+  # 125,:TX,24.00
+  # 126,:TX,44.80
+  # 127,:NC,25.00
+  # 128,:MA,10.00
+  # 129,:CA,102.00
+  # 120,:NC,50.00
+  # Write a function that reads and parses this file and then passes the result to the sales_tax
+  # function. Remember that the data should be formatted into a keyword list and that the fields
+  # need to be the correct types (so the id field is an integer, and so on).
+  #
+  # You'll need the library functions File.open, IO.read(file, :line), and IO.stream(file).
+  def parse_file(filename) do
+    filename
+    |> File.stream!
+    |> Enum.map(&IO.puts/1)
+  end
 end
