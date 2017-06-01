bb1e8b9d554197b13b31ca09557b549deee9f8b4
Sean Marzug-McCarthy <samdec11@gmail.com>
2017-06-01 16:57:26 -0400

    Decode and sort in issues app

--- ch13 - Organizing a Project/issues/lib/issues/cli.ex
+++ ch13 - Organizing a Project/issues/lib/issues/cli.ex
@@ -9,7 +9,9 @@ defmodule Issues.CLI do
   """
 
   def run(argv) do
-    parse_args(argv)
+    argv
+    |> parse_args
+    |> process
   end
 
   @doc """
@@ -33,5 +35,31 @@ defmodule Issues.CLI do
         -> { user, project, @default_count }
       _ -> :help
     end
+  end
+
+  def process(:help) do
+    IO.puts """
+    usage: issues <user> <project> [ count | #{@default_count} ]
+    """
+    System.halt(0)
+  end
+
+  def process({user, project, count}) do
+    Issues.GithubIssues.fetch(user, project)
+    |> decode_response
+    |> sort_into_ascending_order
+    |> Enum.take(count)
+  end
+
+  def decode_response({:ok, body}), do: body
+  def decode_response({:error, error}) do
+    {_, message} = List.keyfind(error, "meesage", 0)
+    IO.puts "Error fetching from Github: #{message}"
+    System.halt(2)
+  end
+
+  def sort_into_ascending_order(list_of_issues) do
+    Enum.sort list_of_issues,
+              fn i1, i2 -> Map.get(i1, "created_at") <= Map.get(i2, "created_at") end
   end
 end
